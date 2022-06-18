b1ed2753dc349d1fc42a5750257cb5b2b707995e
seifElislam <seif.eleslam1990@gmail.com>
2022-06-18 19:55:59 +0200

    Get calendars dates from API

--- src/components/CalendarPage.vue
+++ src/components/CalendarPage.vue
@@ -5,32 +5,32 @@
             <table class="container_dates_table">
                 <tr class="container__weekday">
                     <td colspan="2">
-                        <h1 id="weekday">Monday</h1>
+                        <h1 id="weekday">{{ hijri.weekday }}</h1>
                     </td>
                 </tr>
                 <tr class="container__main_calendars colored">
                     <td class="calendar1">
-                        <h1 id="c1_day" class="day">12</h1>
-                        <div class="frame"><p id="c1_month" class=" highlight">xxx</p></div>
-                        <h2 id="c1_year">2022</h2>
+                        <h1 id="c1_day" class="day">{{ hijri.day }}</h1>
+                        <div class="frame"><p id="c1_month" class="highlight">{{ hijri.month}}</p></div>
+                        <h2 id="c1_year">{{ hijri.year }}</h2>
                     </td>
                     <td class="calendar2">
-                        <h1 id="c2_day" class="day">10</h1>
-                        <div class="frame"> <p id="c2_month" class=" highlight">yyy</p></div>
-                        <h2 id="c2_year">1443</h2>
+                        <h1 id="c2_day" class="day">{{ gregorian.day }}</h1>
+                        <div class="frame"> <p id="c2_month" class="highlight">{{ gregorian.month }}</p></div>
+                        <h2 id="c2_year">{{ gregorian.year }}</h2>
                     </td>
                 </tr>
                 <tr class="container__secondary_calendar">
                     <td class="calendar3" colspan="2">
                         <h2>
-                            <label id="c3_year">1550</label>&nbsp;
-                            <label id="c3_month" class="font" dir="rtl">zzz</label>&nbsp;
-                            <label id="c3_day">10</label>
+                            <label id="c3_year">{{ coptic.year}}</label>&nbsp;
+                            <label id="c3_month" class="font" dir="rtl">{{ coptic.month }}</label>&nbsp;
+                            <label id="c3_day">{{ coptic.day }}</label>
                         </h2>
                     </td>
                 </tr>
                 <tr class="container__quote colored">
-                    <td colspan="2"><h2>quote</h2></td>
+                    <td colspan="2"><h2>{{ quote }}</h2></td>
                 </tr>
             </table>
         </div>
@@ -38,34 +38,50 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'CalendarPage'
+  name: 'CalendarPage',
+  data () {
+    return { coptic: {}, hijri: {}, gregorian: {}, quote: '' }
+  },
+  async mounted () {
+    const apiCall = await axios.get(process.env.VUE_APP_URL + '?lang=ara')
+    this.coptic = apiCall.data.calendars.coptic
+    this.hijri = apiCall.data.calendars.hijri
+    this.gregorian = apiCall.data.calendars.gregorian
+    this.quote = apiCall.data.quote
+  // for (const calendar in apiCall.data.calendars) {
+  //   console.log('------')
+  //   console.log(apiCall.data.calendars[calendar])
+  // }
+  }
 }
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Amiri&display=swap');
 .container {
     direction: rtl;
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100vw;
-    height: 100vh;
+    /* width: 100vw; */
+    /* height: 85vh; */
 
-    background-color: lightgray;
+    /* background-color: lightgray; */
 }
 
 .container__content {
     position: relative;
-    display: flex;
+    /* display: flex; */
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding: var(--default-padding);
     color: var(--color-white);
     z-index: 1;
-    width: 30%;
-    height: 50%;
+    width: 35%;
+    height: 20%;
 }
 
 .container__text {
@@ -79,7 +95,7 @@ export default {
     background-color: whitesmoke;
     box-shadow: 8px 10px 10px 1px rgba(0,0,0,0.5);
     width: 100%;
-    height: 100%;
+    height: 70%;
 }
 
 td {
@@ -94,7 +110,7 @@ td {
     background-color: white;
     box-shadow: 8px 10px 10px 1px rgba(0,0,0,0.5);
     width: 100%;
-    height: 100%;
+    height: 90%;
     border-collapse: collapse;
     border-style: hidden;
     border-radius: 5px;
@@ -102,12 +118,14 @@ td {
 }
 
 .container__weekday {
-    height: 90px;
+    height: 100px;
     border-bottom: 2px solid lightslategray;
+    font-family: 'Amiri', serif;
 }
 
 h1 {
     font-size: xxx-large;
+    margin: 0px
 }
 
 h2 {
@@ -115,7 +133,7 @@ h2 {
 }
 
 .container__main_calendars {
-    height: 250px;
+    height: 200px;
     border-bottom: 2px solid lightslategray;
 }
 
@@ -126,9 +144,12 @@ h2 {
 .container__secondary_calendar {
     height: 100px;
     border-bottom: 2px solid lightslategray;
+    font-family: 'Amiri', serif;
 }
+
 .container__quote {
-    height: 100px;
+    height: 50px;
+    font-family: 'Amiri', serif;
 }
 
 .colored {
@@ -151,10 +172,11 @@ h2 {
     border-radius: 5px;
     text-align:center;
     width: 90%;
-    height: 20%;
+    height: 30%;
     margin: 20px auto;
     border-color: lightslategray;
     background-color: white;
+    font-family: 'Amiri', serif;
   }
 
 </style>
