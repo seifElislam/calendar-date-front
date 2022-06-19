41524ffff2bba88cd826c4e00293cc03181fe56c
seifElislam <seif.eleslam1990@gmail.com>
2022-06-20 01:49:31 +0200

    Add Header to app.js

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav> -->
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active" aria-current="page">Today</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">Search</router-link>
+        </li>
+      </ul>
+  <country-logo/>
+      <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <font-awesome-icon icon="fa-language" />
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="#">EN</a></li>
+            <li><a class="dropdown-item" href="#">ARA</a></li>
+          </ul>
+    </div>
+  </div>
+</nav>
   <router-view/>
 </template>
 
@@ -17,14 +38,16 @@
 
 nav {
   padding: 30px;
+  margin-bottom: 30px
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  /* font-weight: bold; */
+  color: black;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: black
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import CountryLogo from './components/CountryLogo'
 
-createApp(App).use(router).mount('#app')
+library.add(fas)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).component('CountryLogo', CountryLogo).use(router).mount('#app')
